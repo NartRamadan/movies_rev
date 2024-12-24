721e5a06d31d8a81f0f69970ab2ecc30c2cef6c2
NartRamadan <kataz91@gmail.com>
2024-12-24 18:35:04 +0200

    HTML: טופס להזנת שם סרט וביקורת כפתור לחיצה לשליחת נתונים.
    JavaScript:
    לוקח נתונים מהשדות שולח אותם לשרת ב-POST לנתיב /Review בפורמט JSON
    מחכה לתשובה מהשרת עם הביקורת שנוספה.
    בקיצור המשתשמש ממלא טופס לוחץ הוספה והביקרות נשלחת לשרת ונשמרת.
    
    const path = require('path');
    מייבא מודול מובנה path של Node.Js
    המודול הזה מספק כלים לעבודה עם נתיבי קבצים ותקיות
    "file and dirctory paths" בערבית
    
    app.get('/', (req, res) => { ... });
    מגדיר נק' קצה(route)שמאזינה לבקשות HTTP מסוג GET בנתיב הראשי(/)
    
    app.get('/', (req, res) => { ... });
    משתמש במודול path כדי לבנות נתיב מלא לHTML

--- index.js
+++ index.js
@@ -3,30 +3,32 @@ const port = 4371;
 const app = express();
 app.use(express.json());
 
-
-const bodyParser= require('body-parser');
+const bodyParser = require('body-parser');
+const path = require("path");
 app.use(bodyParser.urlencoded({extended: false}));
 
 let curr_id=1;
 let reviews=[];
-let r1={id:curr_id,title:"LOTR",content:"one to rull them all"};
+let r1={id:curr_id,title:"LOTR",content:"one to rule"};
 curr_id++;
 reviews.push(r1);
 
-
 app.post("/Review",(req,res)=>{
-    let item = {};
-    item.id = curr_id;
-    item.title = req.body.title;
+    let item={};
+    item.id      = curr_id;
+    item.title   = req.body.title;
     item.content = req.body.content;
     curr_id++;
     reviews.push(item);
 
-    res.status(200).json(item);
-})
-
-app.get('/Revies',(req,res)=>{
+    res.status(200).json("ok");
+});
+app.get('/Review', (req, res) => {
     res.status(200).json(reviews);
+});
+
+app.get('/', (req, res) => {
+    res.status(200).sendFile(path.join(__dirname,"/views/review_main.html"));
 });
 
 app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
